c4d6a518929471adc551279cedacdfee928c0f4c
Thalles <thallesttt.11@gmail.com>
2024-06-10 19:12:13 -0300

    ajuste apis de estacao requerindo o recurso vaga

--- business/estacaoBusiness.js
+++ business/estacaoBusiness.js
@@ -1,15 +1,26 @@
 const repository = await import("../repository/estacaoRepository");
+const repoBicicleta = await import("../repository/bicicletaRepository");
+const repoVaga = await import("../repository/vagaRepository");
 
 /**Método de aquisição dos objetos do banco via Id
  * @param id Id da requisição
  */
 const findById = async(id = NaN, pool) => {
     let conn;
+    let retorno = 
+        {
+            ok: false, 
+            objeto: null, 
+            count:0
+        };
     try { 
         let query = repository.GetAll();
         conn = await pool.getConnection();
 
         const rows = await conn.query(query);
+        for(let record of retorno.objeto) {
+            reporecord.id
+        }
         return {
             ok: false, objeto: rows, count:0
         };
---
+++ repository/vagaRepository.js
@@ -0,0 +1,37 @@
+/** Sql padrão para captura dos registros da tabela Estacao
+ * @param search parametro de comparação (descricao) capturados por texto 
+ * @param params parametros de comparação 
+*/
+let getAll = (search = "", ...params) => {
+    const condicoes = [];    
+    let stringBuilder = "SELECT A.estacao_id,A.descricao,A.area from vaga A";
+    if(search || params.length > 0) {
+        stringBuilder += "WHERE";     
+    }
+    for(let { key, value } of params) {        
+        condicoes.push(`${key} = ${value}`);
+    }
+    condicoes.forEach(function(condicao) {
+        stringBuilder += condicao;
+    });
+    return stringBuilder;
+}
+
+let getVagasByEstacao = (estacao) => { 
+    return GetAll("", { estacao_id: estacao });
+}
+
+let gravar = (objeto = {}) => {
+    let {estacao_id, descricao, area} = objeto;    
+    let stringBuilder = `INSERT INTO VAGA (estacao_id, descricao, area) 
+                         VALUES (${estacao_id}, '${descricao}', ${area})
+                         ON DUPLICATE KEY UPDATE vaga
+                         set estacao_id = '${estacao_id}', descricao = ${descricao}, area = ${area};`;
+    return stringBuilder;
+}
+
+let remove = (id) => {
+    let stringBuilder = `DELETE FROM vaga WHERE ID = '${id}'`;
+    return stringBuilder;
+}
+export const GetAll = getAll, GetVagasByEstacao = getVagasByEstacao, Gravar = gravar, Delete = remove;
